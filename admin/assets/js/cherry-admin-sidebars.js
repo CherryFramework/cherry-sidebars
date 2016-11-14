c82488ad6ca77cc806af03c3fa52cdd34894ce8b
behaart <behaart@templatemonster.me>
2016-11-14 10:32:00 +0200

    FIX: build #2

--- admin/assets/js/cherry-admin-sidebars.js
+++ admin/assets/js/cherry-admin-sidebars.js
@@ -50,7 +50,7 @@
 		openThickBox: function() {
 			$( 'input[type="text"]', formHandling.form ).removeClass( 'error-invalid' );
 		},
-		submitByttonHandler: function( event ) {
+		submitByttonHandler: function() {
 
 			// Validated form
 			var formData = formHandling.form.serializeArray(),
